c8db687f6be62e3f53fad58ab9e6af65bd82effb
Will Frasher <wfrasher@gmail.com>
2016-03-03 18:28:34 -0500

    code

---
+++ _4.1.19.py
@@ -0,0 +1,23 @@
+﻿
+
+
+
+def pond(years, initialPopulation, harvest,restock):
+    population = initialPopulation
+
+    for year in range(years):
+        population = 1.08 * population - harvest + restock
+        print(year +1, int(population))
+
+
+    return population
+
+
+
+
+
+pond(15,12000,800,100)
+
+
+
+# generalize to allow for the pond to be annually restocked with an additional quantity of fish
